09ce4500523c6f31a5ad6ea22b9627c542343883
Nick Belanger <nickbelanger1324@gmail.com>
2024-12-18 20:47:19 -0600

    Sane nvim, tmux, fzf etc config

---
+++ .bash_aliases
@@ -0,0 +1,23 @@
+# Tools
+alias vim="nvim"
+alias ls="lsd"
+alias tree="lsd --tree"
+# alias man='man "$@" | nvim -R +":set ft=man" - '
+
+# Git stuff
+alias past="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias nuke="git reset --hard && sudo git clean -dfx"
+alias treefix="git submodule deinit --all --force && git submodule update --init --recursive"
+alias bigbang="nuke && treefix"
+
+# Misc Helpers
+alias serve="python -m http.server -d "
+alias q="exit"
+
+function far {
+  find ./ -type f -exec sed -i "s/$1/$2/g" {} \;
+}
+
+function checkpoint {
+  git stash store $(git stash create) -m "$1"
+}
---
+++ .config/nvim/init.lua
@@ -0,0 +1,109 @@
+
+-- General Vim Options
+vim.opt.number = true
+vim.opt.background = light
+vim.opt.clipboard = unnamedplus
+vim.opt.expandtab = true
+-- vim.opt.audoindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+-- vim.opt.colorcolumn = '80'
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.mouse = a
+vim.autoread = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.filetype.add({
+  extension = {
+    sm = 'cpp',
+    dox = 'cpp'
+  },
+  filename = {
+    ['Jenkinsfile'] = 'groovy',
+    ['plugin.txt'] = 'ruby',
+  }
+})
+
+vim.g.load_doxygen_syntax = 1
+
+
+-- Language Support
+require'lspconfig'.clangd.setup{
+  cmd = {'/opt/dclangd'},
+  filetypes = {'c', 'cpp', 'objc', 'objcpp', 'sm', 'doxygen'},
+}
+
+require'lspconfig'.pyright.setup{
+  enabled=true,
+}
+
+vim.opt.signcolumn = 'yes' -- auto/yes/no
+
+require'lsp_lines'.setup{}
+
+vim.diagnostic.config({
+  virtual_text=false,
+  virtual_lines = { only_current_line = true }
+})
+
+require('neogen').setup {
+  languages = {
+    ['cpp.doxygen'] = require('neogen.configurations.cpp'),
+    ['c.doxygen'] = require('neogen.configurations.cpp'),
+    ['doxygen'] = require('neogen.configurations.cpp'),
+  }
+}
+
+require'nvim-treesitter.configs'.setup{
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+vim.treesitter.language.register('cpp', 'sm')
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ['<C-h>'] = 'which_key'
+
+      }
+    }
+  },
+  pickers = {
+    find_files = {
+      no_ignore = true,
+      hidden = true, -- optional, but you probably want this too
+    },
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
+-- Color theme
+require('catppuccin').setup({
+    flavour = os.getenv('CATPPUCCIN_FLAVOR'),
+    transparent_background=true
+})
+
+-- vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme 'catppuccin'
