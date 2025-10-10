802f5ce40d9232951d75503f5db136eceebe50c5
nick <nbelanger@intuitivemachines.com>
2025-10-10 14:59:34 -0500

    Using telescope correctly, setting leader

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,7 @@
 
+-- Leader
+vim.g.mapleader = " "
+
 -- General Vim Options
 vim.opt.number = true
 vim.opt.background = light
@@ -100,6 +103,12 @@ require('telescope').setup{
     -- please take a look at the readme of the extension you want to configure
   }
 }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Color theme
 require('catppuccin').setup({
